544630487303d68fb520cc67993b205bb33be42e
Michael Clausen <michael.clausen@hevs.ch>
2020-12-01 13:55:11 +0100

    Added POC BSP/XCom-CAN BMS data model

--- src/drivers/xcom485i/xcom485ibatterymanager.cpp
+++ src/drivers/xcom485i/xcom485ibatterymanager.cpp
@@ -3,7 +3,32 @@
 
 using namespace std;
 
-XCom485iBatteryManager::XCom485iBatteryManager(XCom485iBatteryManager::Model model): XCom485iDevice(toString(model), 61, {}), model_(model) {}
+XCom485iBatteryManager::XCom485iBatteryManager(XCom485iBatteryManager::Model model): XCom485iDevice(toString(model), 61, {
+    {0, 7000, SIPropertyType::Float, SIPropertyFlag::Readable, "Battery voltage", "Vdc"},
+    {2, 7001, SIPropertyType::Float, SIPropertyFlag::Readable, "Battery current", "Adc"}
+}), model_(model) {
+    switch (model) {
+        case Invalid:
+            break;
+
+        case BSP:
+            addProperties({
+                {2, 6001, SIPropertyType::Float, SIPropertyFlag::Readable | SIPropertyFlag::Writeable, "Nominal capacity", "Ah"},
+                {4, 6002, SIPropertyType::Float, SIPropertyFlag::Readable | SIPropertyFlag::Writeable, "Nominal discharge duration (C-rating)", "h"},
+
+                {110, 6055, SIPropertyType::Float, SIPropertyFlag::Readable | SIPropertyFlag::Writeable | SIPropertyFlag::Expert, "Manufacturer SOC for 0% displayed", "%"},
+                {112, 6056, SIPropertyType::Float, SIPropertyFlag::Readable | SIPropertyFlag::Writeable | SIPropertyFlag::Expert, "Manufacturer SOC for 100% displayed", "%"}
+            });
+            break;
+
+        case XComCAN_BMS:
+            addProperties({
+                {140, 6070, SIPropertyType::Float, SIPropertyFlag::Readable | SIPropertyFlag::Writeable | SIPropertyFlag::Expert, "SOC level under which battery discharge is stopped", "%"},
+                {124, 6062, SIPropertyType::Float, SIPropertyFlag::Readable | SIPropertyFlag::Writeable | SIPropertyFlag::Expert, "SOC level for backup", "%"}
+            });
+            break;
+    }
+}
 
 XCom485iBatteryManager::Model XCom485iBatteryManager::model(XCom485iModbusAccess& access) {
     auto model = access.readInputRegister(61, 122);
