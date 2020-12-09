5b640496f68b115243f33c9ac55d6f74b15deef6
Michael Clausen <michael.clausen@hevs.ch>
2020-12-09 19:27:54 +0100

    Added status for invalid value when writing property

--- src/api/deviceaccess/sistatus.h
+++ src/api/deviceaccess/sistatus.h
@@ -4,7 +4,8 @@ enum class SIStatus: int {
     Success = 0,
     Error = -1,
     NoProperty = -2,
-    Timeout = -3
+    Timeout = -3,
+    InvalidValue = -4
 };
 
 const char* to_string(SIStatus status);
