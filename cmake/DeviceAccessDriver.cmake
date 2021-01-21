7d49fe513befee475f663c427db86de519cd357e
Michael Clausen <michael.clausen@hevs.ch>
2021-01-21 10:03:08 +0100

    Added support to automatically detect version when configuring with CMake

---
+++ cmake/DeviceAccessDriver.cmake
@@ -0,0 +1,6 @@
+function(si_add_device_access_driver name)
+    add_library(${name} MODULE ${ARGN})
+    target_link_libraries(${name} Qt5::Core SIDeviceAccess)
+    set_target_properties(${name} PROPERTIES PREFIX "" SUFFIX ".sdd")
+    install(TARGETS ${name} LIBRARY DESTINATION "var/lib/openstuder/drivers")
+endfunction()
---
+++ cmake/StorageDriver.cmake
@@ -0,0 +1,7 @@
+function(si_add_storage_driver NAME)
+    add_library(${NAME} MODULE)
+    target_sources(${NAME} PRIVATE ${ARGN})
+    target_link_libraries(${NAME} Qt5::Core SIStorage)
+    set_target_properties(${NAME} PROPERTIES PREFIX "" SUFFIX ".ssd")
+    install(TARGETS ${NAME} LIBRARY DESTINATION "var/lib/openstuder/drivers")
+endfunction()
