83558d40987d6c184883cb28708d8bc4c6820872
Michael Clausen <michael.clausen@hevs.ch>
2021-01-26 09:24:20 +0100

    [sigwctl] Allow options in front of commands and add -c option

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,13 +1,17 @@
 #include "siabstractcommand.h"
 #include <QCoreApplication>
 #include <QTextStream>
+#include <QCommandLineParser>
+
+#include <QtDebug>
 
 void usage(QTextStream& output) {
-    output << "Usage: sigwctl <command> ..." << endl << endl;
+    output << "Usage: sigwctl [options] <command> ..." << endl << endl;
     output << "available commands:" << endl;
     for (const auto& command: SIAbstractCommand::allCommands().keys()) {
         output << "  " << command << endl;
     }
+    output << endl << "available options:" << endl << "  -c, --config <dir>  Use configuration location <dir>" << endl;
     output << endl << "use sigwctl help <command> to get help for a specific command." << endl;
 }
 
@@ -20,17 +24,33 @@ int main(int argc, char** argv) {
     QTextStream input(stdin, QIODevice::ReadOnly);
     QTextStream output(stdout, QIODevice::WriteOnly);
 
-    if (QCoreApplication::arguments().count() < 2) {
+    QCommandLineParser commandLineParser;
+    commandLineParser.setOptionsAfterPositionalArgumentsMode(QCommandLineParser::ParseAsPositionalArguments);
+
+    QCommandLineOption configurationLocationOption {{"c", "config"}, "Use configuration location <dir>", "dir"};
+    commandLineParser.addOption(configurationLocationOption);
+
+    if (!commandLineParser.parse(QCoreApplication::arguments())) {
         usage(output);
         return 1;
     }
 
-    auto command = SIAbstractCommand::command(QCoreApplication::arguments()[1]);
+    if (commandLineParser.positionalArguments().count() < 1) {
+        usage(output);
+        return 1;
+    }
+
+    // Options.
+    QVariantMap options;
+    if (commandLineParser.isSet(configurationLocationOption)) {
+        options["c"] = commandLineParser.value(configurationLocationOption);
+    }
+
+    auto command = SIAbstractCommand::command(commandLineParser.positionalArguments().first());
     if (command != nullptr) {
-        auto arguments = QCoreApplication::arguments();
+        auto arguments = commandLineParser.positionalArguments();
         arguments.removeFirst();
-        arguments.removeFirst();
-        return command->run(arguments, input, output);
+        return command->run(arguments, input, output, options);
     } else {
         usage(output);
         return 1;
