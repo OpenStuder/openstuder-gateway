ad725ffa680f46e77772b7c2487ff9f7815ec0a3
Michael Clausen <michael.clausen@hevs.ch>
2021-06-17 15:45:46 +0200

    Installation fixes and improvements

--- cmake/Library.cmake
+++ cmake/Library.cmake
@@ -1,11 +1,11 @@
 function(si_add_library NAME)
     add_library(${NAME} SHARED)
-    install(TARGETS ${NAME} LIBRARY DESTINATION "lib")
+
 endfunction()
 
 function(si_add_library_public_headers NAME INSTALL_POSTFIX)
     target_sources(${NAME} PUBLIC ${ARGN})
-    install(FILES ${ARGN} DESTINATION "include/openstuder/${INSTALL_POSTFIX}")
+    set_target_properties(${NAME} PROPERTIES PUBLIC_HEADER "${ARGN}")
 endfunction()
 
 function(si_add_library_private_headers NAME)
@@ -14,4 +14,13 @@ endfunction()
 
 function(si_add_library_sources NAME)
     target_sources(${NAME} PRIVATE ${ARGN})
+endfunction()
+
+function(si_install_library NAME)
+    install(
+            TARGETS ${NAME}
+            EXPORT ${NAME}_TARGETS
+            LIBRARY DESTINATION "${LIBDIR}"
+            PUBLIC_HEADER DESTINATION "${CMAKE_INSTALL_FULL_INCLUDEDIR}/openstuder/${NAME}"
+    )
 endfunction()
